53f013d5bf0fbcf6322ce33ad20c62830a8a3188
BhattaraiRavi <ravibhattarai@yahoo.com>
2017-05-16 22:11:25 +0000

    Events Functionality Added

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,57 @@
+class EventsController < ApplicationController
+
+	def index
+		@events = Event.all
+	end
+
+	def show
+		@event = Event.find (params[:id])
+	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		respond_to do |format|
+  			if @event.save
+    			format.html { redirect_to @event, notice: 'Event was successfully created.' }
+				format.json { render :show, status: :created, location: @event }
+  			else
+  				format.html { render :new }
+				format.json { render json: @event.errors, status: :unprocessable_entity }
+    		end
+    	end
+	end
+
+	def edit
+		@event = Event.find (params[:id])
+	end
+
+	def update
+		@event = Event.find (params[:id])
+		respond_to do |format|
+			if @event.update(event_params)
+				format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+				format.json { render :show, status: :ok, location: @event }
+			else
+				format.html { render :edit }
+				format.json { render json: @event.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def destroy
+		@event = Event.find (params[:id])
+		@event.destroy
+		redirect_to events_url, notice: 'Event deleted successfully.'
+	end
+
+private
+
+def event_params
+	params.require(:event).permit(:title, :day, :date, :month, :year, :image)
+end
+
+end
